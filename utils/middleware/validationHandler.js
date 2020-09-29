7fa3211bd8951c573e7e88b33fe2e7b0b1f939bf
jairo rivera <jariver92@gmail.com>
2020-09-29 18:46:37 +0200

    install joi and add validate function in validationHandler

--- utils/middleware/validationHandler.js
+++ utils/middleware/validationHandler.js
@@ -1,9 +1,9 @@
-const { check } = require('prettier');
-
 const boom = require('@hapi/boom');
+const joi = require('@hapi/joi');
 
-const validate = () => {
-  return false;
+const validate = (data, schema) => {
+  const { error } = joi.object(schema).validate(data);
+  return error;
 };
 
 const validationHandler = (schema, check = 'body') => {
